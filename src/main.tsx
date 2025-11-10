cfaee68982b7886293d73d65cef824f55d350151
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-11-10 14:56:02 +0800

    init antd

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import '@ant-design/v5-patch-for-react-19';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,7 +1,15 @@
+
+import { Button } from 'antd';
+
+function handleClick() {
+    console.log('click');
+}
+
 export default function User(){
     return (
         <>
             <div>user</div>
+            <Button type="primary" onClick={handleClick}>Login</Button>
         </>
     )
 }
