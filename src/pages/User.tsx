a7b54ae46655cf6470aecbfad1e43bd6f753dfdd
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-12-01 11:26:16 +0800

    feat: like v-if

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,18 +1,42 @@
-
-import { Button } from 'antd';
-import MyButton from "../components/MyButton.tsx";
-
-function handleClick() {
-    console.log('click');
-}
-
 export default function User(){
-    const btnName = 'Test'
+
+    const user:{name:string;age:number;salary:number} = {
+        name:'foo',
+        age:20,
+        salary:100
+    }
+
+    const renderAge = function (){
+        return <>
+            <div>
+                {user.age>=20?bigAge():smallAge()}
+            </div>
+        </>
+    }
+
+    const bigAge = function (){
+        return (
+            <>
+                <div>It is big age</div>
+            </>
+        )
+    }
+
+    const smallAge = function (){
+        return (
+            <>
+                <div>It is small age</div>
+            </>
+        )
+    }
+
     return (
         <>
-            <div>user</div>
-            <Button type="primary" onClick={handleClick}>Login</Button>
-           <MyButton title={btnName}></MyButton>
+            <div>
+                <span>{user.name}</span>
+                <div>实现一下v-if的效果</div>
+                <div>{renderAge()}</div>
+            </div>
         </>
     )
 }
